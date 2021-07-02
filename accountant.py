009aab47c5d119b20bc23200fcc1267f1c298ed7
kajankowska <85677666+kajankowska@users.noreply.github.com>
2021-07-02 22:48:32 +0200

    Update accountant.py

--- accountant.py
+++ accountant.py
@@ -18,18 +18,17 @@ actions = ["balance", "sales", "purchase"]
 
 print("Jaką operację chcesz rozpocząć? Do wyboru: balance, sales lub purchase")
 input(str())
-    if act in actions:
-        if act = actions[0]:
-            print(int("Zmiana na koncie: {}"))
-            print(str("Komentarz do zmiany: {}"))
-        if input == actions[1]:
-            print(str("ID produktu: {}"))
-            print(int("Cena jednostkowa: {}"))
-            print(int("Liczba sztuk: {}"))
-        if input == actions[2]:
-            print(str("ID produktu: {}"))
-            print(int("Cena jednostkowa: {}"))
-            print(int("Liczba sztuk: {}"))
+    if input = actions[0]:
+        print(int("Zmiana na koncie: {}"))
+        print(str("Komentarz do zmiany: {}"))
+    if input == actions[1]:
+        print(str("ID produktu: {}"))
+        print(int("Cena jednostkowa: {}"))
+        print(int("Liczba sztuk: {}"))
+    if input == actions[2]:
+        print(str("ID produktu: {}"))
+        print(int("Cena jednostkowa: {}"))
+        print(int("Liczba sztuk: {}"))
 
 
 
