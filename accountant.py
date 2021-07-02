9a8badaacaf5564f58c1eb476d15ab0e0b316aba
Kasia Jankowska <ksobinska@gmail.com>
2021-07-02 22:36:40 +0200

    Initial commit

---
+++ accountant.py
@@ -0,0 +1,39 @@
+# System księgowy
+# Rejestrowanie operacji na koncie
+
+import sys
+
+print(sys.argv)
+
+balance = 0
+sales = {}
+purchase = {}
+history = {}
+
+account = 0
+warehouse = {}
+review = 0
+
+actions = ["balance", "sales", "purchase"]
+
+print("Jaką operację chcesz rozpocząć? Do wyboru: balance, sales lub purchase")
+input(str())
+    if input == actions[0]:
+        print(int("Zmiana na koncie: {}"))
+        print(str("Komentarz do zmiany: {}"))
+    if input == actions[1]:
+        print(str("ID produktu: {}"))
+        print(int("Cena jednostkowa: {}"))
+        print(int("Liczba sztuk: {}"))
+    if input == actions[2]:
+        print(str("ID produktu: {}"))
+        print(int("Cena jednostkowa: {}"))
+        print(int("Liczba sztuk: {}"))
+
+
+
+
+
+
+
+
