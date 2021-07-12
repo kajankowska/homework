6ff578286039ee9af9388a8078050edae661b230
kajankowska <85677666+kajankowska@users.noreply.github.com>
2021-07-12 17:29:54 +0200

    Delete helloworld.py

--- helloworld.py
+++
@@ -1,7 +0,0 @@
-base = 800
-promo = 100
-a, b, c, d, e, f, g, h, i, j, k, l, m, n, o = 32, 23, 54, 23, 64, 94, 93, 23, 23, 95, 11, 32, 23, 43, 93,
-message = "Forecast całkowity w tygodniu wynosi {}.".format((base + prmo) * a)
-message2 = "Forecast całkowity w tygodniu wynosi {}.".format(b)
-message3 = "Forecast całkowity w tygodniu wynosi {}.".format(c)
-print(message, message2, message3)
