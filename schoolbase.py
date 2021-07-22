970824190ed08c5618e8b105c9d7f5af0b6d3012
kajankowska <85677666+kajankowska@users.noreply.github.com>
2021-07-22 16:56:14 +0200

    input added

--- schoolbase.py
+++ schoolbase.py
@@ -6,91 +6,102 @@ import sys
 phrase = sys.argv[1:]
 print(sys.argv)
 
-# variables
+# lists and dictionaries:
 
-name = ()
-educator = ()
+educators = []
 teachers = []
 students = []
 class_list = []
+
 school_class = {}
-class_choice = ()
+data = {}
 
-user = ["uczeń", "nauczyciel", "wychowawca", "koniec"]
-
-
-class SchoolClasses:
-
-    def __init__(self, school_class):
-        self.school_class = school_class
-        self.educator = input()
-        self.teachers = []
-        self.students = []
-
-    def class_choice(self):
-        class_list = []
-        while True:
-            print("Wprowadź numer klasy")
-            class_nr = input()
-            if class_nr == "":
-                return class_list
-            if class_nr not in school_class.keys():
-                school_class[class_nr] = SchoolClasses(class_nr)
-            class_list.append(class_nr)
+users = "uczeń", "nauczyciel", "wychowawca", "koniec"
 
 
 class Educator:
 
     def __init__(self, name):
         self.name = name
+        self.school_class = self.class_choice()
+        self.class_list = []
 
-    def class_assign(self):
-        self.class_nr = class_choice
-        for k in self.class_nr:
-            k.educator = self.name
+
+    def class_choice(self):
+        while True:
+            class_nr = input("\nWprowadź numer klasy: \nJeśli nie ma więcej danych do wprowadzenia, wciśnij *ENTER*\n")
+            if class_nr == "":
+                return class_list
+            if class_nr not in school_class:
+                school_class[class_nr] = class_nr
+                class_list.append(class_nr)
+                educators.append(name)
 
 
 class Teacher:
 
     def __init__(self, name):
         self.name = name
+        self.subject = self.subject_choice()
+        self.school_class = self.class_choice()
+        self.class_list = []
 
-    def class_assign(self):
-        self.subject = input()
-        self.school_class = class_choice
-        for k in self.class_nr:
-            k.teachers.append(self.name)
+    def subject_choice(self):
+        subject = input("\nWprowadź nazwę przedmiotu, którego uczy {}:\n".format(self.name))
+        return subject
+
+    def class_choice(self):
+        while True:
+            class_nr = input("\nWprowadź numer klasy: \nJeśli nie ma więcej danych do wprowadzenia, wciśnij *ENTER*\n")
+            if class_nr == "":
+                return class_list
+            if class_nr not in school_class:
+                school_class[class_nr] = class_nr
+            class_list.append(class_nr)
+            teachers.append(name)
 
 
 class Student:
 
     def __init__(self, name):
         self.name = name
+        self.class_nr = self.class_choice()
+        students.append(name)
 
-    def class_assign(self):
-        class_nr = input()
-        if class_nr not in school_class.keys():
-            school_class[class_nr] = SchoolClasses(class_nr)
-        self.class_nr = SchoolClasses(class_nr)
-        self.class_nr.students.append(self.name)
+    def class_choice(self):
+        self.class_nr = input("\nWprowadź nazwę klasy, do której uczęszcza {}\n".format(self.name))
+
 
 while True:
-    users = input("\nWybierz rodzaj użytkownika. \nDo wyboru: uczeń, nauczyciel, wychowawca."
-                  "\n* Jeśli chcesz zakończyć, wprowadź: koniec *\n")
+    user = input("\nWybierz rodzaj użytkownika: uczeń, nauczyciel lub wychowawca.\nAby zakończyć wprowadź: koniec\n")
     if user == "koniec":
-        print("\nWprowadzanie zakończone. Przejdź do podsumowania.")
+        print("\nWprowadzanie zakończone.\nPodsumowanie:\n")
         break
-    elif user not in users:
+    if user not in users:
         print("\nNiepoprawne dane. Wprowadź ponownie")
         continue
     else:
-        name = input("Podaj imię i nazwisko użytkownika")
+        name = input("\nPodaj imię i nazwisko użytkownika:\n")
         if user == "uczeń":
-            student = input("Wprowadź nazwę klasy, do której uczęszcza {}".format(name))
+            student = Student(name)
             continue
         if user == "nauczyciel":
-            subject = input("Wprowadź nazwę przedmiotu, którego uczy {}".format(name))
+            teacher = Teacher(name)
+            continue
         if user == "wychowawca":
-            class_list
+            educator = Educator(name)
+            continue
+
+
+if phrase[0] in class_list:
+    print("Wychowawcą klasy {} jest .\nLista uczniów klasy:\n" .format(phrase[0]))
+if phrase[0] in educators:
+    print("Wychowawca {} jest opiekunem poniższych uczniów.\n" .format(phrase[0]))
+if phrase[0] in teachers:
+    print("Nauczyciel {} prowadzi lekcje w klasach, których wychowawcami są:\n" .format(phrase[0]))
+if phrase[0] in students:
+    print("Przedmioty ucznia {} to .\n" .format(phrase[0]))
+
+
 
 
