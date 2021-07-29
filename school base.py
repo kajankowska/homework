a02560fe525a67e323cada624b750259016cb4f0
kajankowska <85677666+kajankowska@users.noreply.github.com>
2021-07-29 11:34:14 +0200

    global dict add

--- school base.py
+++ school base.py
@@ -80,6 +80,8 @@ class Student:
     def __init__(self, name):
         self.name = name
         self.student_assign()
+        self.class_list = []
+        self.class_list.append(self.class_nr)
 
     def student_assign(self):
         data[self.name] = self
@@ -92,7 +94,7 @@ class Student:
 while True:
     user = input("\nWybierz rodzaj użytkownika: uczeń, nauczyciel lub wychowawca.\nAby zakończyć wprowadź: koniec\n")
     if user == "koniec":
-        print("\nWPROWADZANIE ZAKOŃCZONE.\nPODSUMOWANIE:\n")
+        print("\nWprowadzanie zakończone.\nPODSUMOWANIE:\n")
         break
     if user not in users:
         print("\nNiepoprawne dane. Wprowadź ponownie")
@@ -111,30 +113,25 @@ while True:
 
 # Execution:
 
-# phrase specified class_nr:
-if phrase[0] in data:
+if len(phrase[0]) == 2:  # phrase specified class_nr:
     print(f"Informacje o klasie {phrase[0]}:\n")
     if data[phrase[0]].educator:
         print(f"Wychowawcą klasy {phrase[0]} jest {data[phrase[0]].educator}.")
     if data[phrase[0]].students_list:
         print(f"Lista uczniów klasy: {data[phrase[0]].students_list}")
-
-# phrase educator name:
-if phrase[0] in data:
+else:  # phrase educator name:
     print(f"Wychowawca {phrase[0]} jest opiekunem poniższych uczniów:")
     if data[phrase[0]].class_list:
         for class_nr in data[phrase[0]].class_list:
             print(data[class_nr].students_list)
 
-# phrase teacher name:
-if phrase[0] in data:
+if phrase[0] in data:  # phrase teacher name:
     print(f"Lista wychowawców klas, z którymi {phrase[0]} prowadzi zajęcia:")
     if data[phrase[0]].class_list:
         for class_nr in data[phrase[0]].class_list:
             print(data[class_nr].educator)
 
-# phrase student name:
-if phrase[0] in data:
+if phrase[0] in data:  # phrase student name:
     print(f"Lista zajęć (wraz z nauczycielami prowadzącymi), na które uczęszcza {phrase[0]}:")
-    for class_nr in data[phrase[0]].class_nr:
-        print(data[class_nr].educator)
+    for class_nr in data[phrase[0]].class_list:
+        print(data[class_nr].teachers)
