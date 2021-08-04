e69283a4f0adcdd6740015e9f2be5faf22f552dc
kajankowska <85677666+kajankowska@users.noreply.github.com>
2021-08-04 23:08:53 +0200

    beginning

---
+++ reader.py
@@ -0,0 +1,23 @@
+import sys
+import os
+import csv
+
+
+src_filepath = sys.argv[1]
+dst_filepath = sys.argv[2]
+changes = sys.argv[3:]
+print(sys.argv)
+
+fields = []
+rows = []
+
+
+with open("src_filepath", newline="") as f:
+    reader = csv.reader(f)
+    for line in reader:
+        print(" ".join(line))
+
+    if not os.path.exists("src.filepath"):
+        print("Nie można odnaleźć pliku!\n" + f"Zawartość wskazanej lokalizacji:\n{os.listdir(src_filepath)}.")
+    else:
+        print(f"Plik znajduje się w lokalizacji {src_filepath}.")
