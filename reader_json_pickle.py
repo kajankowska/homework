eb64e9e3d7b66db63fcdb59037b18f5620086506
kajankowska <85677666+kajankowska@users.noreply.github.com>
2021-08-28 00:53:15 +0200

    create classes

--- reader_json_pickle.py
+++ reader_json_pickle.py
@@ -5,62 +5,50 @@ import json
 import pickle
 import pathlib
 
-src_filepath = sys.argv[1]  # filetype: csv, json or pickle
+src_filepath = sys.argv[1]
 dst_filepath = sys.argv[2]
 newdata = sys.argv[3:]
 changes = []
 fields = []
 
-if os.path.exists(src_filepath):
-    print(f"\nŚcieżka do pliku: {src_filepath}.\n")
+
+class File:
+    def __init__(self):
+        self.input_file = src_filepath
+        self.output_file = dst_filepath
+        self.newdata = sys.argv[3:]
+        self.changes = []
+        self.fields = []
+        for change in self.newdata:
+            self.changes.append(change.split(","))
+
+    def file_editing(self):
+        for change in self.changes:
+            if int(change[0]) > len(self.newdata[0]):
+                print("Wrong number of rows!")
+                return False
+            if int(change[1]) > len(self.newdata[0]):
+                print("Wrong number of columns!")
+                return False
+            self.fields[int(change[0])][int(change[1])] = change[2]
 
 
-class FileReading():
+class CsvFile:
+    def __init__(self):
+        super().__init__()
 
-    def csv_type(input):
-        with open((sys.argv[1]), "r", newline="", encoding="utf-8") as csvfile:
+    def editing(self):
+        with open(self.src_file, "r", newline="", encoding="utf-8") as csvfile:
             reader = csv.reader(csvfile)
             for row in reader:
-                fields.append(row)
-
-    def json_type(input):
-        with open(sys.argv[1], "r") as file:
-            input += json.loads(file.read())
-            return True
-
-    def pickle_type(input):
-        with open(sys.argv[1], "rb") as file:
-            input += pickle.loads(file.read())
-            return True
-
-    if src_filepath == "csv":
-        csv_type(input)
-    if src_filepath == "json":
-        json_type(input)
-    if src_filepath == "csv":
-        pickle_type(input)
+                self.fields.append(row)
 
 
-if not os.path.exists(src_filepath) or not os.path.isfile(src_filepath):
-    print(f"\nNie można odnaleźć pliku!\n" + f"Zawartość wskazanej lokalizacji:\n{os.listdir()}")
+class Json:
+    def __init__(self):
+        super().__init__()
 
-for change in sys.argv[3:]:
-    changes.append(change.split(","))
 
-for change in changes:
-    if int(change[0]) > len(newdata[0]):
-        print("Błędna liczba wierszy!")
-        exit()
-    if int(change[1]) > len(newdata[0]):
-        print("Błędna liczba kolumn!")
-        exit()
-    fields[int(change[0])][int(change[1])] = change[2]
-
-for i in fields:
-    print(i)
-
-with open((sys.argv[2]), "w", newline="", encoding="utf-8") as csvoutfile:
-    writer = csv.writer(csvoutfile)
-    for row in fields:
-        writer.writerow(row)
-print(f"\nPlik został zapisany w lokalizacji: {dst_filepath}.")
+class Pickle:
+    def __init__(self):
+        super().__init__()
